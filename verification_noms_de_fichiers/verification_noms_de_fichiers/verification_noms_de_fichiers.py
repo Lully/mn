e2f93871ccfd3b2410de98a7c32e4821595c529a
Lullly <lully1804@yahoo.fr>
2018-12-04 11:26:15 +0100

    initialisation du script "verification_noms_de_fichiers"

---
+++ verification_noms_de_fichiers/verification_noms_de_fichiers/verification_noms_de_fichiers.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+"""
+Vérification du nommage des fichiers d'un répertoire
+"""
+
+import re
+
+def analyse_dir(directory_name):
+
+
+if __name__ == "__main__":
+    directory_name = input("Chemin du répertoire où analyser les noms des fichiers : ")
+    analyse_dir(directory_name)
